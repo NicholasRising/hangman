1de1a4140130b6713fce1335e46e02cf8027bd92
Nicholas Rising <nicholas.rising@gmail.com>
2022-09-30 01:53:04 +0100

    Display a dictionary entry when the game concludes and make everything a bit fancier

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
+import json
 import random
+import requests
 import string
 import time
 
@@ -37,11 +39,52 @@ class Game:
 
         clear_screen()
         if not fancy:
-            print(out)
+            print(f'{out}\n')
         else:
             for line in out.split('\n'):
                 print(line)
                 time.sleep(0.05)
+
+def get_entry(word):
+    return json.loads(requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}').text)
+
+def has_definition(entry):
+    return not ('title' in entry and entry['title'] == 'No Definitions Found')
+                
+def draw_entry(entry):
+    out = f'\033[1m{entry["word"].capitalize()}\033[0m\n'
+    if 'phonetic' in entry:
+        out += f'\033[37m{entry["phonetic"]}\033[0m\n'
+    out += '\n'
+
+    for index_a, meaning_a in enumerate(entry['meanings']):
+        for relative_index, meaning_b in enumerate(entry['meanings'][index_a + 1:]):
+            if meaning_a['partOfSpeech'] == meaning_b['partOfSpeech']:
+                entry['meanings'].pop(index_a + relative_index + 1)
+
+    for meaning in entry['meanings']:
+        out += f'\033[4m{meaning["partOfSpeech"].capitalize()}\033[0m\n\n'
+        for definition in meaning['definitions']:
+            out += f'     - {definition["definition"].capitalize()}\n'
+            if 'example' in definition:
+                out += f'\n      \033[3m\033[37m"{definition["example"].capitalize()}"\033[0m\n'
+            if definition['synonyms']:
+                out += '\n      Synonyms\n'
+
+                for synonym in definition['synonyms']:
+                    out += f'          \033[37m- "{synonym}"\033[0m\n' 
+            if definition['antonyms']:
+                out += '\n      Antonyms\n'
+
+                for antonym in definition['antonyms']:
+                    out += f'          \033[37m- "{antonym}"\033[0m\n'
+            out += '\n'
+
+    out += f'\033[4mSources\033[0m\n'
+    for source in entry['sourceUrls']:
+        out += f'    \033[37m  {source}\033[0m\n'
+
+    print(out)
 
 def clear_screen():
     print('\033[1J\033[H', end = '')
@@ -56,26 +99,35 @@ def main():
 
     if single_player:
         answer = list(words)[random.randrange(len(words))]
+        entry = get_entry(answer)
+
+        while not has_definition(entry):
+            answer = list(words)[random.randrange(len(words))]
+            entry = get_entry(answer)
     else:
         clear_screen()
         answer = input('Target word: ').lower()
+        entry = get_entry(answer)
     
-        while answer not in words:
+        while answer not in words or not has_definition(entry):
             clear_screen()
-            input('Please enter a valid English word\n\n\033[37mPress Enter to continue\033[0m')
+            input('Please enter a valid English word\n\n\033[37mPress Enter to continue...\033[0m')
 
             clear_screen()
             answer = input('Target word: ')
+            entry = get_entry(answer)
      
+    entry = entry[0]
+
     game = Game(answer, hangmen)
-    game.draw(fancy = True)
+    game.draw(fancy = False)
 
     while game.status() == 'ongoing':
         guess = input('\nNext guess: ').lower()
 
         while len(guess) != 1 or guess not in string.ascii_lowercase or guess in game.guesses:
             clear_screen()
-            input('Please enter a single unguessed character (a-z)\n\n\033[37mPress Enter to continue\033[0m')
+            input('Please enter a single unguessed character (a-z)\n\n\033[37mPress Enter to continue...\033[0m')
 
             game.draw()
             guess = input('Next guess: ').lower()
@@ -90,11 +142,14 @@ def main():
         print(end_text[0])
     elif game.status() == 'lost':
         print(end_text[1])
-        time.sleep(2)
-        clear_screen()
-        print(f'The correct word was \"{game.answer}\"')
 
     time.sleep(2)
+    clear_screen()
+
+    draw_entry(entry)
+     
+    input('\033[37mPress Enter to exit...\033[0m')
+    clear_screen()
 
 if __name__ == '__main__':
     main()
